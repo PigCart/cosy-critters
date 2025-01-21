67ea7f52bb3b03081881e676563e284149295a22
PIGC <PigCart@outlook.com>
2025-01-21 17:28:23 +0000

    better mixins, fix daytime bug

--- src/main/java/pigcart/cosycritters/mixin/MothSpawningBlockMixin.java
+++ src/main/java/pigcart/cosycritters/mixin/MothSpawningBlockMixin.java
@@ -4,22 +4,19 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseTorchBlock;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import pigcart.cosycritters.Cosycritters;
 
-@Mixin(BaseTorchBlock.class)
-public class BaseTorchBlockMixin extends Block {
+import java.lang.reflect.Array;
 
-    public BaseTorchBlockMixin(Properties properties) {
-        super(properties);
-    }
+@Mixin(BaseTorchBlock.class)
+public class BaseTorchBlockMixin extends BlockMixin {
+
+
 
     @Override
-    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-        super.animateTick(state, level, pos, random);
-        System.out.println("hello?");
+    public void spawnCritters(BlockState state, Level level, BlockPos pos, RandomSource random) {
         Cosycritters.trySpawnMoth(level, pos);
     }
 }
